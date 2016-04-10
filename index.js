cd35a0efbd70000f3d09e4814afa05ae5bb7dd57
Daniel Husar <dano.husar@gmail.com>
2016-04-10 12:48:43 +0100

    1.0.2

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+var exposedProperties = ['window', 'navigator', 'document'];
+
 global.document = require('jsdom').jsdom('<body></body>');
 global.window = document.defaultView;
-global.navigator = window.navigator;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
 
+global.navigator = {
+  userAgent: 'node.js'
+};
